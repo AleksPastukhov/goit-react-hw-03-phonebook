7f15b9880878dffed2192f69d73f648574f292e4
AleksPastukhov <passandro1990@gmail.com>
2023-02-17 17:28:56 +0200

    adds lifecycle methods

--- src/components/App/App.jsx
+++ src/components/App/App.jsx
@@ -5,12 +5,7 @@ import { Wrapper } from './App.styled';
 
 class App extends Component {
   state = {
-    contacts: [
-      { id: 'id-1', name: 'Rosie Simpson', number: '459-12-56' },
-      { id: 'id-2', name: 'Hermione Kline', number: '443-89-12' },
-      { id: 'id-3', name: 'Eden Clements', number: '645-17-79' },
-      { id: 'id-4', name: 'Annie Copeland', number: '227-91-26' },
-    ],
+    contacts: [],
     filter: '',
   };
 
@@ -34,10 +29,33 @@ class App extends Component {
     };
   };
 
+  componentDidMount() {
+    const contacts = localStorage.getItem('contacts');
+    const parsedContacts = JSON.parse(contacts);
+    if (parsedContacts) {
+      this.setState({
+        contacts: parsedContacts,
+      });
+    }
+  }
+
+  componentDidUpdate(prevProps, prevState) {
+    if (this.state.contacts !== prevState.contacts) {
+      localStorage.setItem('contacts', JSON.stringify(this.state.contacts));
+    }
+  }
+
   formSubmitHendler = ({ name, number }) => {
     for (let contact of this.state.contacts) {
-      if (contact.name === name) {
+      if (
+        contact.name.toLowerCase() === name.toLowerCase() &&
+        contact.number === number
+      ) {
         alert(`${name} is already in contacts`);
+        return;
+      }
+      if (contact.number === number) {
+        alert(`${number} is already in contacts`);
         return;
       }
     }
@@ -48,6 +66,7 @@ class App extends Component {
 
   render() {
     const { contacts, filter } = this.state;
+    // localStorage.setItem('contacts', JSON.stringify(this.state.contacts));
     return (
       <Wrapper>
         <PhonebookSection
